c2e22ebd4d90c1b4e7a0836c698ba565f0a715ed
Johnathan Warlick <johnathw@netapp.com>
2023-03-07 06:44:49 +0000

    Swap wget for curl in bootstrap

--- _bootstrap.sh
+++ _bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd /root/
-wget https://raw.githubusercontent.com/johnwarlick/netapp-ontap-ansible-awx-demo/main/_bootstrap.yml
+curl -O https://raw.githubusercontent.com/johnwarlick/netapp-ontap-ansible-awx-demo/main/_bootstrap.yml
 ansible-playbook _bootstrap.yml
