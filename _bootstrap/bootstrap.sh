9dc660bb95bb235a6a22677d5d7a56d749ec6531
Johnathan Warlick <johnathw@netapp.com>
2023-03-07 22:37:39 +0000

    Custom venv dir has to be IN the container

--- _bootstrap/bootstrap.sh
+++ _bootstrap/bootstrap.sh
@@ -2,11 +2,12 @@
 python3 -m venv bootstrap
 source bootstrap/bin/activate
 pip install --upgrade pip setuptools
-pip install ansible
+pip install ansible requests docker
 ansible-galaxy collection install awx.awx:17.0.0 
 read -s -p "Password for AWX: " TOWER_PASS
 read -s -p "Password for ONTAP Clusters: " ONTAP_PASS
 export ONTAP_PASSWORD=$ONTAP_PASS
 export TOWER_PASSWORD=$TOWER_PASS
 curl https://raw.githubusercontent.com/johnwarlick/netapp-ontap-ansible-awx-demo/main/_bootstrap/bootstrap.yml -o bootstrap.yml
+curl https://raw.githubusercontent.com/johnwarlick/netapp-ontap-ansible-awx-demo/main/_bootstrap/.tower_cli.cfg.j2 -o .tower_cli.cfg.j2
 ansible-playbook bootstrap.yml
