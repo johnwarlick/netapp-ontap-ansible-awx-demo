c7057f0b3f55fd83942e72244421405ee4efaa11
Johnathan Warlick <johnathw@netapp.com>
2023-03-08 22:46:34 -0600

    Refactor bootstrap to include mock app

--- _bootstrap/bootstrap.sh
+++ _bootstrap/bootstrap.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-python3 -m venv bootstrap
-source bootstrap/bin/activate
+cd /root
+python3 -m venv venv
+source venv/bin/activate
 pip install --upgrade pip setuptools
 pip install ansible requests docker
+# This lab has a pretty old AWX install
 ansible-galaxy collection install awx.awx:17.0.0 
 read -s -p "Password for AWX: " TOWER_PASS
 read -s -p "Password for ONTAP Clusters: " ONTAP_PASS
 export ONTAP_PASSWORD=$ONTAP_PASS
 export TOWER_PASSWORD=$TOWER_PASS
-curl https://raw.githubusercontent.com/johnwarlick/netapp-ontap-ansible-awx-demo/main/_bootstrap/bootstrap.yml -o bootstrap.yml
-curl https://raw.githubusercontent.com/johnwarlick/netapp-ontap-ansible-awx-demo/main/_bootstrap/.tower_cli.cfg.j2 -o .tower_cli.cfg.j2
+git clone https://github.com/johnwarlick/netapp-ontap-ansible-awx-demo.git
+cd netapp-ontap-ansible-awx-demo/_bootstrap
 ansible-playbook bootstrap.yml
